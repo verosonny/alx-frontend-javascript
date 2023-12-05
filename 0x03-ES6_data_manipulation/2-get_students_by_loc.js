3c5e0b641a907e9e0a621335c3d2e9cdbc7d961d
Mwadali <wellingtonmwadali@gmail.com>
2023-12-05 12:31:27 -0800

    2-get_students_by_loc.js

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,14 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
